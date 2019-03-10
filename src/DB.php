80ba9db79cca8f0f2fdf1cfdaa2cb2f7e43d95e0
Leon <leongrdic@gmail.com>
2019-03-10 18:18:26 +0100

    improved transaction handling, removed custom exceptions, added debug

--- src/DB.php
+++ src/DB.php
@@ -1,79 +1,37 @@
-<?php
+<?php namespace Le;
 // php-db-pdo-mysql by Leon, MIT License
 
-namespace Le;
+final class DB{
+	public static $debug = false;
 
-Exception::register(20, [
-	11 => "Invalid database host, expecting string",
-	12 => "Invalid database port, expecting number larger than 0",
-	13 => "Invalid database name, expecting string",
-	14 => "Invalid database user, expecting string",
-	15 => "Invalid database password, expecting string",
-	16 => "Invalid database prefix, expecting string",
-	17 => "Invalid database charset, expecting string",
-	18 => "Database connection error",
-	19 => "Invalid condition column name with index ?, expecting non-empty string",
-	20 => "Invalid condition value for column '?', expecting string, number, bool or null",
-	21 => "Invalid additional option 'order', expecting string",
-	22 => "Invalid additional option 'limit', expecting number larger than 0",
-	23 => "Invalid additional option 'offset', expecting positive number",
-	24 => "Invalid data column name with index ?, expecting non-empty string",
-	25 => "Data value count doesn't match column count on entry with index ?",
-	26 => "Invalid data value for column '?', expecting string, number, bool or null",
-	27 => "Invalid table name, expecting string",
-	28 => "Invalid columns list, expecting string",
-	29 => "Invalid conditions list, expecting array or string",
-	30 => "Invalid additional parameter, expecting array",
-	31 => "Invalid data list, expecting non-empty array",
-	32 => "Database query error: ?"
-]);
+	private static function parseColumns($columns){
+		if(empty($columns)) return '';
+		if(is_string($columns)) return $columns;
 
-class DB{
-	private $db, $prefix, $return_query, $transaction = false;
-
-	function __construct($argoptions = []){
-		$options = ['host' => 'localhost', 'port' => 3306, 'database' => '', 'user' => '', 'password' => '', 'return_query' => false];
-		$options = array_replace($options, $argoptions);
-
-		if(!is_string($options['host']) || empty($options['host'])) throw new Exception(20, 11);
-		if(!is_numeric($options['port']) || $options['port'] < 1) throw new Exception(20, 12);
-		if(!is_string($options['database']) || empty($options['database'])) throw new Exception(20, 13);
-		if(!is_string($options['user'])) throw new Exception(20, 14);
-		if(!is_string($options['password'])) throw new Exception(20, 15);
-
-		if(isset($options['prefix']) && (!is_string($options['prefix']) || empty($options['prefix']))) throw new Exception(20, 16);
-		if(isset($options['charset']) && (!is_string($options['charset']) || empty($options['charset']))) throw new Exception(20, 17);
-
-		$this->return_query = $options['return_query'];
-		$this->prefix = (isset($options['prefix'])) ? $options['prefix'] : '';
-
-		try{
-			$this->db = new \PDO('mysql:host=' . $options['host'] . ';port=' . $options['port'] . ';dbname=' . $options['database'], $options['user'], $options['password']);
-		}catch(\PDOException $error){
-			throw new Exception(20, 18, [], $error);
+		foreach($columns as $index => $column){
+			if(self::$debug && (!is_string($column) && !is_numeric($column) || empty($column))) throw new Error("DEBUG: invalid column name with index " . $index);
+			$columns[$index] = $this->escapeName($column);
 		}
 
-		$this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-		if(isset($options['charset']) && !empty($options['charset'])) $this->db->exec('SET CHARACTER SET ' . $options['charset']);
+		$query = implode(', ', $columns);
+		return $query;
 	}
 
-	function __destruct(){
-		$this->db = null;
-	}
-
-	private function conditionsParse($conditions){
+	private static function parseConditions($conditions){
 		if(empty($conditions)) return '';
-		if(is_string($conditions)) return 'WHERE ' . $conditions;
+		if(is_string($conditions)) return ' WHERE ' . $conditions;
 
 		$query = '';
 		$index = 0;
 		foreach($conditions as $column => $value){
-			if(!is_string($column) && !is_numeric($column) || empty($column)) throw new Exception(20, 19, [$index]);
-			if(!is_string($value) && !is_numeric($value) && !is_bool($value) && !is_null($value)) throw new Exception(20, 20, [$column]);
+			if(self::$debug){
+				if(!is_string($column) && !is_numeric($column) || empty($column)) throw new Error("DEBUG: invalid column name with index " . $index);
+				if(!is_string($value) && !is_numeric($value) && !is_bool($value) && !is_null($value)) throw new Error("DEBUG: invalid value for column " . $column);
+			}
 
 			if(is_bool($value)) $value = $value ? 1 : 0;
 			$column = $this->escapeName($column);
-			
+
 			if($query) $query .= ' AND ';
 
 			if(is_null($value)){
@@ -89,22 +47,22 @@ class DB{
 		return ' WHERE ' . $query;
 	}
 
-	private function additionalParse($additional){
+	private static function parseAdditional($additional){
 		$query = '';
 
 		if(isset($additional['order'])){
-			if(!is_string($additional['order'])) throw new Exception(20, 21);
+			if(self::$debug && !is_string($additional['order'])) throw new Error("DEBUG: invalid additional order");
 			if($additional['order']) $query .= ' ORDER BY ' . $additional['order'];
 		}
-		
+
 		if(isset($additional['single']) && $additional['single']) $additional['limit'] = 1;
 
 		if(isset($additional['limit'])){
-			if(!is_numeric($additional['limit']) || $additional['limit']<1) throw new Exception(20, 22);
+			if(self::$debug && (!is_numeric($additional['limit']) || $additional['limit']<1)) throw new Error("DEBUG: invalid additional limit");
 			$query .= ' LIMIT ' . $additional['limit'];
 
 			if(isset($additional['offset'])){
-				if(!is_numeric($additional['offset']) || $additional['offset']<0) throw new Exception(20, 23);
+				if(self::$debug && (!is_numeric($additional['offset']) || $additional['offset']<0)) throw new Error("DEBUG: invalid additional offset");
 				$query .= ' OFFSET ' . $additional['offset'];
 			}
 		}
@@ -112,7 +70,7 @@ class DB{
 		return $query;
 	}
 
-	private function dataParse($data, $single_row = false){
+	private static function parseData($data, $single_row = false){
 		$query = '';
 		$values = '';
 
@@ -120,7 +78,7 @@ class DB{
 		if(isset($data[0]) && is_array($data[0]) && !$single_row){ // format: 0 => [column names], 1 => [row values], 2 => ...
 			$count = count($data[0]);
 			foreach($data[0] as $index => $column){
-				if(!is_string($column) && !is_numeric($column) || empty($column)) throw new Exception(20, 24, [$index]);
+				if(self::$debug && (!is_string($column) && !is_numeric($column) || empty($column))) throw new Error("DEBUG: invalid column name with index " . $index);
 				$data[0][$index] = $this->escapeName($column);
 			}
 			$query = '(' . implode(', ', $data[0]) . ') VALUES ';
@@ -128,10 +86,10 @@ class DB{
 			foreach($data as $id => $row){
 				if($id === 0) continue; // skipping column names
 
-				if(count($row) != $count) throw new Exception(20, 25, [$id]);
+				if(count($row) != $count) throw new Error("DEBUG: value count doesn't match column count on id " . $id);
 
 				foreach($row as $column_id => $value){
-					if(!is_string($value) && !is_numeric($value) && !is_bool($value) && !is_null($value)) throw new Exception(20, 26, [$data[0][$column_id]]);
+					if(self::$debug && !is_string($value) && !is_numeric($value) && !is_bool($value) && !is_null($value)) throw new Error("DEBUG: invalid value for column " . $data[0][$column_id]);
 
 					if(is_bool($value)) $value = $value ? 1 : 0;
 
@@ -146,8 +104,10 @@ class DB{
 			$query = ' SET ';
 			$index = 0;
 			foreach($data as $column => $value){
-				if(!is_string($column) && !is_numeric($column) || empty($column)) throw new Exception(20, 24, [$index]);
-				if(!is_string($value) && !is_numeric($value) && !is_bool($value) && !is_null($value)) throw new Exception(20, 26, [$column]);
+				if(self::$debug){
+					if(!is_string($column) && !is_numeric($column) || empty($column)) throw new Error("DEBUG: invalid column with index " . $index);
+					if(!is_string($value) && !is_numeric($value) && !is_bool($value) && !is_null($value)) throw new Error("DEBUG: invalid value for column " . $column);
+				}
 
 				if(is_bool($value)) $value = $value ? 1 : 0;
 
@@ -165,14 +125,48 @@ class DB{
 		return $query;
 	}
 
-	public function schema($table){
-		$query = 'DESCRIBE ' . $this->prefix . $table;
+	// OBJECT
 
-		try{
-			$result = $this->db->query($query);
-		}catch(\PDOException $error){
-			throw new Exception(20, 32, [$query], $error);
+	private $db, $prefix, $return_query, $transaction = 0;
+
+	function __construct($argoptions = []){
+		$options = ['host' => 'localhost', 'port' => 3306, 'database' => '', 'user' => '', 'password' => '', 'return_query' => self::$debug];
+		$options = array_replace($options, $argoptions);
+
+		if(self::$debug){
+			if(!is_string($options['host']) || empty($options['host'])) throw new Error("DEBUG: invalid host");
+			if(!is_numeric($options['port']) || $options['port'] < 1) throw new Error("DEBUG: invalid port");
+			if(!is_string($options['database']) || empty($options['database'])) throw new Error("DEBUG: invalid db name");
+			if(!is_string($options['user'])) throw new Error("DEBUG: invalid username");
+			if(!is_string($options['password'])) throw new Error("DEBUG: invalid password");
+
+			if(isset($options['prefix']) && (!is_string($options['prefix']) || empty($options['prefix']))) throw new Error("DEBUG: invalid table prefix");
+			if(isset($options['charset']) && (!is_string($options['charset']) || empty($options['charset']))) throw new Error("DEBUG: invalid charset");
 		}
+
+		$this->return_query = $options['return_query'];
+		$this->prefix = (isset($options['prefix'])) ? $options['prefix'] : '';
+
+		$this->db = new \PDO('mysql:host=' . $options['host'] . ';port=' . $options['port'] . ';dbname=' . $options['database'], $options['user'], $options['password']);
+		$this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+		if(isset($options['charset']) && !empty($options['charset'])) $this->db->exec('SET CHARACTER SET ' . $options['charset']);
+	}
+
+	function __destruct(){
+		$this->db = null;
+	}
+
+	public function escape($string){
+		return $this->db->quote($string);
+	}
+
+	public function escapeName($string){
+		return '`' . str_replace('`', '\\`', $string) . '`';
+	}
+
+	public function schema($table){
+		$query = 'DESCRIBE ' . $this->escapeName($this->prefix . $table);
+		$result = $this->db->query($query);
 
 		$data = []; $id = 0;
 		while($data[$id] = $result->fetch(\PDO::FETCH_ASSOC)) $id++;
@@ -187,18 +181,15 @@ class DB{
 	}
 
 	public function get($table, $columns = '*', $conditions = [], $additional = []){
-		if(!is_string($table)) throw new Exception(20, 27);
-		if(!is_string($columns)) throw new Exception(20, 28);
-		if(!is_array($conditions) && !is_string($conditions)) throw new Exception(20, 29);
-		if(!is_array($additional)) throw new Exception(20, 30);
-
-		$query = 'SELECT ' . $columns . ' FROM ' . $this->escapeName($this->prefix . $table) . $this->conditionsParse($conditions) . $this->additionalParse($additional);
-
-		try{
-			$result = $this->db->query($query);
-		}catch(\PDOException $error){
-			throw new Exception(20, 32, [$query], $error);
+		if(self::$debug){
+			if(!is_string($table)) throw new Error("DEBUG: invalid table");
+			if(!is_array($conditions) && !is_string($conditions)) throw new Error("DEBUG: invalid columns");
+			if(!is_array($conditions) && !is_string($conditions)) throw new Error("DEBUG: invalid conditions");
+			if(!is_array($additional)) throw new Error("DEBUG: invalid additional");
 		}
+
+		$query = 'SELECT ' . self::parseColumns($columns) . ' FROM ' . $this->escapeName($this->prefix . $table) . self::parseConditions($conditions) . self::parseAdditional($additional);
+		$result = $this->db->query($query);
 
 		$data = [];
 		if($result->rowCount() > 0){
@@ -206,7 +197,7 @@ class DB{
 			while($data[$id] = $result->fetch(\PDO::FETCH_ASSOC)) $id++;
 			unset($data[$id]);
 
-			if($result->rowCount() === 1 && isset($additional['single']) && $additional['single']) $data = $data[0];
+			if(isset($additional['single']) && $additional['single']) $data = $data[0];
 		}
 
 		$output = [
@@ -218,16 +209,13 @@ class DB{
 	}
 
 	public function insert($table, $data){
-		if(!is_string($table)) throw new Exception(20, 27);
-		if(!is_array($data) || empty($data)) throw new Exception(20, 31);
-
-		$query = 'INSERT INTO ' . $this->escapeName($this->prefix . $table) . $this->dataParse($data);
-
-		try{
-			$result = $this->db->query($query);
-		}catch(\PDOException $error){
-			throw new Exception(20, 32, [$query], $error);
+		if(self::$debug){
+			if(!is_string($table)) throw new Error("DEBUG: invalid table");
+			if(!is_array($data) || empty($data)) throw new Error("DEBUG: invalid data");
 		}
+
+		$query = 'INSERT INTO ' . $this->escapeName($this->prefix . $table) . self::parseData($data);
+		$result = $this->db->query($query);
 
 		$output = [
 			'count' => $result->rowCount(),
@@ -238,18 +226,15 @@ class DB{
 	}
 
 	public function update($table, $data, $conditions = [], $additional = []){
-		if(!is_string($table)) throw new Exception(20, 27);
-		if(!is_array($data) || empty($data)) throw new Exception(20, 31);
-		if(!is_array($conditions) && !is_string($conditions)) throw new Exception(20, 29);
-		if(!is_array($additional)) throw new Exception(20, 30);
-
-		$query = 'UPDATE ' . $this->escapeName($this->prefix . $table) . $this->dataParse($data, true) . $this->conditionsParse($conditions) . $this->additionalParse($additional);
-
-		try{
-			$result = $this->db->query($query);
-		}catch(\PDOException $error){
-			throw new Exception(20, 32, [$query], $error);
+		if(self::$debug){
+			if(!is_string($table)) throw new Error("DEBUG: invalid table");
+			if(!is_array($data) || empty($data)) throw new Error("DEBUG: invalid data");
+			if(!is_array($conditions) && !is_string($conditions)) throw new Error("DEBUG: invalid conditions");
+			if(!is_array($additional)) throw new Error("DEBUG: invalid additional");
 		}
+
+		$query = 'UPDATE ' . $this->escapeName($this->prefix . $table) . self::parseData($data, true) . self::parseConditions($conditions) . self::parseAdditional($additional);
+		$result = $this->db->query($query);
 
 		$output = ['count' => $result->rowCount()];
 		if($this->return_query) $output['query'] = $query;
@@ -257,17 +242,14 @@ class DB{
 	}
 
 	public function delete($table, $conditions = [], $additional = []){
-		if(!is_string($table)) throw new Exception(20, 27);
-		if(!is_array($conditions) && !is_string($conditions)) throw new Exception(20, 29);
-		if(!is_array($additional)) throw new Exception(20, 30);
-
-		$query = 'DELETE FROM ' . $this->escapeName($this->prefix . $table) . $this->conditionsParse($conditions) . $this->additionalParse($additional);
-
-		try{
-			$result = $this->db->query($query);
-		}catch(\PDOException $error){
-			throw new Exception(20, 32, [$query], $error);
+		if(self::$debug){
+			if(!is_string($table)) throw new Error("DEBUG: invalid table");
+			if(!is_array($conditions) && !is_string($conditions)) throw new Error("DEBUG: invalid conditions");
+			if(!is_array($additional)) throw new Error("DEBUG: additional");
 		}
+
+		$query = 'DELETE FROM ' . $this->escapeName($this->prefix . $table) . self::parseConditions($conditions) . self::parseAdditional($additional);
+		$result = $this->db->query($query);
 
 		$output = ['count' => $result->rowCount()];
 		if($this->return_query) $output['query'] = $query;
@@ -275,19 +257,21 @@ class DB{
 	}
 
 	public function transactionBegin(){
-		if($this->transaction) return true;
+		if($this->transaction){
+			$this->transaction++;
+			return true;
+		}
 
 		$this->db->beginTransaction();
-		$this->transaction = true;
-
+		$this->transaction = 1;
 		return true;
 	}
 
 	public function commit(){
 		if(!$this->transaction) return false;
 
-		$this->db->commit();
-		$this->transaction = false;
+		if($this->transaction === 1) $this->db->commit();
+		$this->transaction--;
 
 		return true;
 	}
@@ -296,16 +280,8 @@ class DB{
 		if(!$this->transaction) return false;
 
 		$this->db->rollBack();
-		$this->transaction = false;
+		$this->transaction = 0;
 
 		return true;
-	}
-
-	public function escape($string){
-		return $this->db->quote($string);
-	}
-	
-	public function escapeName($string){
-		return '`' . str_replace('`', '\\`', $string) . '`';
 	}
 }
